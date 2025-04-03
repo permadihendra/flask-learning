84659fd90e2a3bd72d76d7cf4c10475dae6c4c51
permadihendra <hendrapermadi69@gmail.com>
2025-04-03 21:27:08 +0700

    add event handler on forms

--- frontend/src/pages/Auth/SignUp.js
+++ frontend/src/pages/Auth/SignUp.js
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const SignUp = () => {
-  return (
 
-    <div className="signUpContainer">
-    <form>
-    <h2>Create an Account</h2>
-      <div className="signUpForm">
-        <label htmlFor="name">Name</label>
-        <input id="name" type="text"  name="name" />
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const nameHandler = (e) => {
+        setName(e.target.value);
+    };
 
-      <label htmlFor="email">Email Address</label>
-        <input id="email" type="email"  name="email" />
+    const emailHandler = (e) => {
+        setEmail(e.target.value);
+    }
 
-       <label htmlFor="password">Password</label>
-        <input  id="password" type="password" name="password"  />
-            <button>Register</button>
-                  </div>
+    const passwordHandler = (e) => {
+        setPassword(e.target.value);
+    }
 
-    </form>
-    </div>
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        alert(`Name: ${name}: Email: ${email} Password: ${password}`);
+    };
 
-  );
+    return (
+
+        <div className="signUpContainer">
+        <form onSubmit={onSubmitHandler}>
+        <h2>Create an Account</h2>
+        <div className="signUpForm">
+            <label htmlFor="name">Name</label>
+            <input 
+                id="name" 
+                type="text"  
+                name="name" 
+                value={name} 
+                onChange={nameHandler}/>
+
+        <label htmlFor="email">Email Address</label>
+            <input 
+                id="email" 
+                type="email"  
+                name="email" 
+                value={email}
+                onChange={emailHandler}
+                />
+
+        <label htmlFor="password">Password</label>
+            <input  
+                id="password" 
+                type="password" 
+                name="password"  
+                value={password}
+                onChange={passwordHandler}
+                />
+                
+                <button>Register</button>
+                    </div>
+
+        </form>
+        </div>
+
+    );
 };
 
 export default SignUp;
