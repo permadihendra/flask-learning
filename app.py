db514bc09c61e78c9908351a9ed30a8d36af8468
permadihendra <hendrapermadi69@gmail.com>
2025-04-04 15:33:32 +0700

    add and update speakers api endpoint by passing variable

--- app.py
+++ app.py
@@ -13,14 +13,29 @@ def venues():
         "name": "Auditorium A"
     }), 200
 
-@app.route('/api/v1/speakers/')
-def speakers():
-    firstname = request.args.get("firstname")
-    lastname = request.args.get("lastname")
-    if firstname is not None and lastname is not None:
-        return jsonify(message="The speaker's fullname : "+firstname+" "+lastname)
+# @app.route('/api/v1/speakers/')
+# def speakers():
+#     firstname = request.args.get("firstname")
+#     lastname = request.args.get("lastname")
+#     if firstname is not None and lastname is not None:
+#         return jsonify(message="The speaker's fullname : "+firstname+" "+lastname)
+#     else:
+#         return jsonify(message="No query parameters in the url")
+
+@app.route('/api/v1/speakers/<int:speakerId>')
+def getSpeaker(speakerId):
+     # Simulated data lookup
+    speakers = {
+        1: {"id": 1, "name": "John Doe", "topic": "AI"},
+        2: {"id": 2, "name": "Jane Smith", "topic": "Cybersecurity"}
+    }
+
+    speakerData = speakers.get(speakerId)
+
+    if speakerData:
+        return jsonify(speakerData)
     else:
-        return jsonify(message="No query parameters in the url")
+        return jsonify({"error": "Speaker not found"}), 404
 
 if __name__ == '__main__':
     app.run()
